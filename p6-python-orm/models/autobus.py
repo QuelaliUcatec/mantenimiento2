4eda013b034a5b5a84c615ac3fe4e72e54072a0c
Fernando Saavedra <saavedravargas624@gmail.com>
2026-06-16 18:38:09 -0400

    Limpieza: eliminando carpetas antiguas P7-FleetControl y p6-python-orm

--- p6-python-orm/models/autobus.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
-
-class Autobus(Base):
-    __tablename__ = "autobuses"
-    
-    id = Column(Integer, primary_key=True, index=True)
-    marca = Column(String)
-    placa = Column(String, unique=True)
